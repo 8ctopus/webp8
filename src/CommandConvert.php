28c4e20edb6002f3d05a245728ab9d591f791c0e
8ctopus <hello@octopuslabs.io>
2020-07-09 09:16:29 +0500

    renamed: command convert class

--- src/CommandConvert.php
+++ src/CommandConvert.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Stopwatch\Stopwatch;
 
-class NewCommand extends Command
+class CommandConvert extends Command
 {
     /**
      * Configure command options
