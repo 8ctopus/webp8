2aab9bdcb435e64c011af2af2e1da75121a0f841
8ctopus <hello@octopuslabs.io>
2023-06-28 14:59:30 +0400

    Build 1.0.0

--- src/EntryPoint.php
+++ src/EntryPoint.php
@@ -15,7 +15,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     throw new Exception('autoload not found');
 }
 
-$app = new Application('webp8', '0.1.8');
+$app = new Application('webp8', '1.0.0');
 $app->add(new CommandConvert());
 $app->add(new CommandCleanup());
 
