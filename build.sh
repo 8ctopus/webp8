7447197d4b63dc8314c9b11e66aa72a88bf1aa01
8ctopus <hello@octopuslabs.io>
2023-07-24 15:18:38 +0400

    Sync build script from self sign

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
-# remove development dependencies from phar
-composer install --no-dev
-
 #php src/BuildPhar.php
 php box.phar compile
 
 $(php -r "file_put_contents('bin/webp8.sha256', hash_file('sha256', 'bin/webp8.phar'));")
-
-composer install
